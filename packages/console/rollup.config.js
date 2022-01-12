97a9794b82a0ec4d8a1d09ce3ab69d73eea0228b
minhdc <minhdc@fabbi.io>
2022-01-12 11:15:03 +0700

    updated rollup-package.json-tsconfig

--- packages/console/rollup.config.js
+++ packages/console/rollup.config.js
@@ -1,36 +1,51 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import typescript from 'rollup-plugin-typescript';
+import { terser } from "rollup-plugin-terser";
+//
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+
 import pkg from './package.json';
-export default {
-  external: Object.keys(pkg['dependencies'] || []),
-  input: './src/index.ts',
-  plugins: [
-    typescript(),
-    resolve({
-      mainFields: [ "browser", "module", "main" ],
-      preferBuiltins: false
-  }),
-    commonjs()
-  ],
-  output: [
-    // ES module (for bundlers) build.
-    {
-      format: 'esm',
-      file: pkg.module,
-      sourcemap: true
-    },
-    // CommonJS (for Node) build.
-    {
-      format: 'cjs',
-      file: pkg.main,
-      sourcemap: true
-    },
-    // browser-friendly UMD build
-    {
-      format: 'umd',
-      file: pkg.browser,
-      name: "_console"
-    }
-  ]
-}
+
+export default [
+	// browser-friendly UMD build
+	{
+		input: 'src/index.ts',
+		output: {
+			name: '_console',
+			file: pkg.browser,
+			format: 'umd',
+			sourcemap: true
+		},
+		plugins: [
+			nodeResolve(),   // so Rollup can find `ms`
+			commonjs(),  // so Rollup can convert `ms` to an ES module
+			typescript({
+				lib: ["es5", "es6", "dom"],
+				target: "es5"
+			}) // so Rollup can convert TypeScript to JavaScript
+		]
+	},
+
+	// CommonJS (for Node) and ES module (for bundlers) build.
+	// (We could have three entries in the configuration array
+	// instead of two, but it's quicker to generate multiple
+	// builds from a single configuration where possible, using
+	// an array for the `output` option, where we can specify 
+	// `file` and `format` for each target)
+	{
+		input: 'src/index.ts',
+		external: Object.keys(pkg['dependencies'] || []),
+		plugins: [
+      		terser({module: true}),
+			nodeResolve(),
+			typescript({
+				lib: ["es5", "es6", "dom"],
+				target: "es5"
+			}) // so Rollup can convert TypeScript to JavaScript
+		],
+		output: [
+			{ file: pkg.main, format: 'cjs', sourcemap: true},
+			{ file: pkg.module, format: 'es', sourcemap: true}
+		]
+	}
+];
